c8c8eb3bece96d19dc99bee35db51e9969730c4d
kay <kaynguyen.dev@gmail.com>
2019-12-12 12:54:45 -0500

    created & styled Card component

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,17 @@ import React, { useState } from "react";
 import logo from "./logo.png";
 import "./App.scss";
 
+import Card from "./components/Card/Card";
+
 const App = () => {
   const [logoToggling, setLogoToggling] = useState(true);
+  const [cards, setCards] = useState([
+    { id: 1 },
+    { id: 2 },
+    { id: 3 },
+    { id: 4 },
+    { id: 5 }
+  ]);
 
   const toggleLogo = () => {
     logoToggling === true ? setLogoToggling(false) : setLogoToggling(true);
@@ -14,11 +23,19 @@ const App = () => {
     <div className="app">
       <img
         src={logo}
-        className={logoToggling === true ? "static-logo" : "animated-logo"}
+        className={
+          logoToggling === true ? "app-logo-static" : "app-logo-animated"
+        }
         alt="logo"
         onClick={toggleLogo}
       />
       <h1>VitaStore</h1>
+
+      <div className="app-grid">
+        {cards.map(card => (
+          <Card duration={150} key={card.id} />
+        ))}
+      </div>
     </div>
   );
 };
