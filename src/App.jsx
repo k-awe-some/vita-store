b5333d692c6e88e85c0b0c7a11e7efc07cc81ec5
kay <kaynguyen.dev@gmail.com>
2019-12-12 12:53:43 -0500

    created toggle logo animation

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 
+import logo from "./logo.png";
 import "./App.scss";
 
-const App = () => (
-  <div>
-    <h1>VitaStore</h1>
-  </div>
-);
+const App = () => {
+  const [logoToggling, setLogoToggling] = useState(true);
+
+  const toggleLogo = () => {
+    logoToggling === true ? setLogoToggling(false) : setLogoToggling(true);
+  };
+
+  return (
+    <div className="app">
+      <img
+        src={logo}
+        className={logoToggling === true ? "static-logo" : "animated-logo"}
+        alt="logo"
+        onClick={toggleLogo}
+      />
+      <h1>VitaStore</h1>
+    </div>
+  );
+};
 
 export default App;
